d666d3c6ef2897b1d88a3aecba7bbb496683a8f0
Cieliocas <francielioevangelista@hotmail.com>
2024-12-17 20:38:49 -0300

    Teste tipagens array

--- src/tipos_basicos.ts
+++ src/tipos_basicos.ts
@@ -9,9 +9,10 @@ let altura: number = 1.75 // number
 const nacionalidade: string = 'brasileiro' // string
 
 // Teste de arrays
-
+// tipos de arrays - string
 const colegas: string[] = ['cielio', 'joao', 'maria'] // array de strings
 
+// Outra forma de declarar um array tipado
 const tecnologias: Array<string> = ['javascript', 'typescript', 'nodejs'] // array de strings
 
 
