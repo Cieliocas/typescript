706c2eeb511b63ea5edb72b22b5ceff1bb7686c1
Cieliocas <francielioevangelista@hotmail.com>
2024-12-17 20:36:04 -0300

    Teste tipagens

--- src/tipos_basicos.ts
+++ src/tipos_basicos.ts
@@ -1,3 +1,11 @@
-let estaChovendo: boolean = true
+let estaChovendo: boolean = true // boolean
 
 estaChovendo = 'true' // erro de compilação devido ao tipo da variável
+
+let idade: number = 23 // number
+
+let altura: number = 1.75 // number
+
+const nacionalidade: string = 'brasileiro' // string
+
+
