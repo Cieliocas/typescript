486ba8f826a7970686fa537495298eb689185460
Cieliocas <francielioevangelista@hotmail.com>
2024-12-18 19:45:00 -0300

    casting para tipos com unknown

--- src/casting.ts
+++ src/casting.ts
@@ -8,4 +8,7 @@ namespace Casting { // problema resolvido
     (idade as string[]).forEach(x => { // casting de idade para string[], 
         console.log(x); // erro em tempo de execução
     }) // os metodos são aplicaveis quando o casting é feito para os tipos corretos
+    
+    let nome: string = 35 as unknown as string; // casting de number para string
+    // o unknown é um tipo que aceita qualquer valor, mas não permite que você faça nada com ele
 }
