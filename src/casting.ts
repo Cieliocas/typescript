632f384c6f8c70baa7a6470d340d6ba37cf95957
Cieliocas <francielioevangelista@hotmail.com>
2024-12-18 19:41:47 -0300

    casting para tipos

--- src/casting.ts
+++ src/casting.ts
@@ -1,6 +1,11 @@
-let idade: any = 25; 
+// let idade: any = 25; 
 // está retornando erro pois a variável idade está sendo declarada em outro arquivo
 
 namespace Casting { // problema resolvido
     let idade: any = 25; 
+    (idade as number).toFixed() // casting de idade para number
+    (idade as string).length // casting de idade para string
+    (idade as string[]).forEach(x => { // casting de idade para string[], 
+        console.log(x); // erro em tempo de execução
+    }) // os metodos são aplicaveis quando o casting é feito para os tipos corretos
 }
