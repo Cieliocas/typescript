2592292cb67ffbb5497cdfa302f2b2ef70158d05
Cieliocas <francielioevangelista@hotmail.com>
2024-12-18 19:29:08 -0300

    funcoes arrow

--- src/funcoes.ts
+++ src/funcoes.ts
@@ -2,3 +2,6 @@
 function calculaArea(base: number, altura: number): number {
     return base * altura;
 }
+
+// const com arrow
+const calculaArea2 = (base: number, altura: number): number => base * altura;
