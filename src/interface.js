6ae20636eca79aa08d96a42589c05b0660ce0a01
Cieliocas <francielioevangelista@hotmail.com>
2024-12-18 22:49:28 -0300

    add interface definitions with comments explaining their purpose in TypeScript

---
+++ src/interface.js
@@ -0,0 +1,5 @@
+"use strict";
+// interfaces no typescript são como contratos, 
+// onde você define quais propriedades e métodos uma classe deve ter
+// para implementar uma interface, 
+// você deve seguir o contrato definido por ela
