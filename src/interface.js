7fa31ed936563f8f19ded3cc9ff894557a8d63eb
Cieliocas <francielioevangelista@hotmail.com>
2024-12-18 23:05:06 -0300

    add Conta and ContaCorrente classes with interface implementation and deposit/transfer methods

--- src/interface.js
+++ src/interface.js
@@ -3,3 +3,29 @@
 // onde você define quais propriedades e métodos uma classe deve ter
 // para implementar uma interface, 
 // você deve seguir o contrato definido por ela
+class Conta {
+    constructor(numeroConta) {
+        this.saldo = 0;
+        this.numeroConta = numeroConta;
+    }
+}
+class ContaSalario extends Conta {
+    depositar(valor) {
+        this.saldo += valor;
+    }
+}
+// utilizando o implements para implementar a interface ao invés de extends
+// o implements é usado para implementar interfaces 
+// ao invés de herdar propriedades e métodos
+class ContaCorrente extends Conta {
+    constructor() {
+        super(...arguments);
+        this.taxaTransferencia = 0; // propriedade
+    }
+    // implementando a interface   
+    transferir(valor, contaDestino) {
+        contaDestino.saldo += (valor - this.taxaTransferencia); // lógica
+        return true; // retorno devido ao contrato
+    }
+    ;
+}
--- src/interface.ts
+++ src/interface.ts
@@ -3,6 +3,38 @@
 // para implementar uma interface, 
 // você deve seguir o contrato definido por ela
 
+class Conta {
+    numeroConta: number;
+    saldo: number = 0;
 
+    constructor(numeroConta: number) {
+        this.numeroConta = numeroConta;
+    }
+}
+
+
+class ContaSalario extends Conta {
+    depositar(valor: number) {
+        this.saldo += valor;
+    }
+}
+
+// começando a interface
+interface ITransacional { // contrato
+    transferir: (valor: number, contaDestino: Conta) => boolean; // método
+    taxaTransferencia: number; // propriedade
+}
+
+// utilizando o implements para implementar a interface ao invés de extends
+// o implements é usado para implementar interfaces 
+// ao invés de herdar propriedades e métodos
+class ContaCorrente extends Conta implements ITransacional { 
+    // implementando a interface   
+    transferir(valor: number, contaDestino: Conta) { // método
+        contaDestino.saldo += (valor - this.taxaTransferencia); // lógica
+        return true; // retorno devido ao contrato
+    };
+    taxaTransferencia: number = 0; // propriedade
+}
 
 
